92ff1085440b94deb49f4a9030cf1a32d79622a8
bochap <moibochap@gmail.com>
2024-11-11 22:17:52 -0500

    allow access from localhost for service_api container and add image for zip-code worker

--- service/api/run.py
+++ service/api/run.py
@@ -3,4 +3,6 @@ import sys
 
 if __name__ =="__main__":
     sys.argv.insert(1, "service.api.server:app")
+    sys.argv.append("--host")
+    sys.argv.append("0.0.0.0")  # Listen on all interfaces    
     sys.exit(uvicorn.main())
