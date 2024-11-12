e617b0e0aa45b31314fc60b03f9666296012a86c
bochap <moibochap@gmail.com>
2024-11-12 00:43:41 -0500

    fix certificate issue

--- library/webclient/rest.py
+++ library/webclient/rest.py
@@ -1,8 +1,11 @@
 from typing import Any, Dict
+import certifi
+import ssl
 from aiohttp import ClientSession, TCPConnector
 
 async def get_to_json(url: str) -> Dict[str, Any]:
-    async with ClientSession(connector=TCPConnector(verify_ssl=False)) as session:
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+    async with ClientSession(connector=TCPConnector(ssl=ssl_context)) as session:
         async with session.get(url) as response:
             response.raise_for_status()
             return await response.json()
